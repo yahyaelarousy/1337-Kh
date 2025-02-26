588e2db9a1bffe0bd7e7c374bb5aa44cc7b84aee
YelArou <adilhabibi728@gmail.com>
2025-02-26 23:35:25 +0000

    done

--- c00/ex00/ft_putchar.c
+++ c00/ex00/ft_putchar.c
@@ -4,7 +4,7 @@ void ft_putchar(char c)
     write(1, &c, 1);
     }
 int main ()
-    {
+{
 ft_putchar('p');
 
-    }
+}
---
+++ c04/ex02/ft_putnbr.c
@@ -0,0 +1,26 @@
+#include<unistd.h>
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putnbr(int nb)
+{
+	if (nb == -2147483648)
+	{
+		write(1, "-2147483648", 11);
+		return ;
+	}
+	if (nb < 0)
+	{
+		ft_putchar('-');
+		nb = nb * -1;
+	}
+	if (nb >= 0 && nb < 10)
+		ft_putchar(nb + '0');
+	else
+	{
+		ft_putnbr(nb / 10);
+		ft_putnbr(nb % 10);
+	}
+}
